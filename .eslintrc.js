c238294cb0978241bf83c7df01572237c7300811
Walid Widocode <walid69guestin@gmail.com>
2023-02-22 15:18:19 +0100

    update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
     env: {
         browser: true,
@@ -28,34 +27,3 @@ module.exports = {
         },
     },
 };
-=======
-module.exports = {
-    env: {
-        browser: true,
-        es6: true,
-    },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    globals: {
-        Atomics: 'readonly',
-        SharedArrayBuffer: 'readonly',
-    },
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
-    plugins: ['react', 'react-hooks'],
-    rules: {
-        'react/prop-types': 0,
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-    },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-    },
-};
->>>>>>> e910fc8b3767c385573eee70fe23865612865da6
